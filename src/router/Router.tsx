56cd3b30ebe8675115a52013a31fffb0ff158334
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2021-11-22 15:06:13 +0900

    add file

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router';
 import { Home } from '../components/pages/Home';
 import { Page404 } from '../components/pages/Page404';
 import { Works } from '../components/pages/Works';
-import workstest from '../components/pages/workstest';
+import { workstest } from '../components/pages/workstest';
 import Informations from '../components/pages/Informations';
 import { HeaderLayout } from '../components/templates/HeaderLayout';
 
